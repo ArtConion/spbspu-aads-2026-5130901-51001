28e24a1cc2b25da70d56e4e21f0be5c6395d4b7e
ArtConion <artiom0408@mail.ru>
2026-05-16 19:20:02 +0300

    fix build 2

--- vishnyakov.artiom/S3/siphash.hpp
+++ vishnyakov.artiom/S3/siphash.hpp
@@ -4,6 +4,7 @@
 #include <boost/container_hash/hash.hpp>
 #include <cstddef>
 #include <string>
+#include <utility>
 
 namespace vishnyakov
 {
@@ -13,6 +14,14 @@ namespace vishnyakov
     std::size_t operator()(const std::string& key) const
     {
       return boost::hash< std::string >()(key);
+    }
+
+    template < class T1, class T2 >
+    std::size_t operator()(const std::pair< T1, T2 >& key) const
+    {
+      std::size_t h1 = (*this)(key.first);
+      std::size_t h2 = (*this)(key.second);
+      return h1 ^ (h2 << 1);
     }
   };
 }

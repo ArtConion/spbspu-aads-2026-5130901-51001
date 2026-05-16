abbb8fed196e06362dd8aca734124e3d7e85a9e8
ArtConion <artiom0408@mail.ru>
2026-05-16 17:55:57 +0300

    implement file parsing and main function

--- vishnyakov.artiom/S3/main.cpp
+++ vishnyakov.artiom/S3/main.cpp
@@ -1,13 +1,83 @@
-#include "hash_table.hpp"
+#include "command.hpp"
+#include "graph.hpp"
+#include "siphash.hpp"
+#include "list.hpp"
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+
+namespace vishnyakov
+{
+  void parse_graph_file(std::istream& file, CommandHandler& handler)
+  {
+    std::string line;
+
+    while (std::getline(file, line))
+    {
+      if (line.empty())
+      {
+        continue;
+      }
+
+      std::istringstream iss(line);
+      std::string graph_name;
+      std::size_t edge_count;
+
+      iss >> graph_name >> edge_count;
+
+      Graph graph;
+
+      for (std::size_t i = 0; i < edge_count; ++i)
+      {
+        if (!std::getline(file, line))
+        {
+          throw std::runtime_error("Unexpected end of file");
+        }
+
+        if (line.empty())
+        {
+          --i;
+          continue;
+        }
+
+        std::istringstream edge_iss(line);
+        std::string from;
+        std::string to;
+        unsigned long long weight;
+
+        edge_iss >> from >> to >> weight;
+
+        graph.add_edge(from, to, weight);
+      }
+
+      handler.add_graph(graph_name, graph);
+    }
+  }
+
+  void process_commands(std::istream& in, CommandHandler& handler, std::ostream& out)
+  {
+    std::string line;
+
+    while (std::getline(in, line))
+    {
+      if (line.empty())
+      {
+        continue;
+      }
+
+      handler.execute(line, out);
+    }
+  }
+}
 
 int main(int argc, char* argv[])
 {
+  using namespace vishnyakov;
+
   if (argc != 2)
   {
-    std::cerr << "Usage: " << argv[0] << " <filename>" << '\n';
+    std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
     return 1;
   }
 
@@ -15,7 +85,20 @@ int main(int argc, char* argv[])
 
   if (!file.is_open())
   {
-    std::cerr << "Error: Cannot open file " << argv[1] << '\n';
+    std::cerr << "Error: Cannot open file " << argv[1] << std::endl;
+    return 1;
+  }
+
+  CommandHandler handler;
+
+  try
+  {
+    parse_graph_file(file, handler);
+    process_commands(std::cin, handler, std::cout);
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl;
     return 1;
   }
 

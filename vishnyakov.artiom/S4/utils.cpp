d7f769865b4c2db9a4e4da94cf35009edd2b7633
ArtConion <artiom0408@mail.ru>
2026-05-17 19:28:04 +0300

    fix: correct parse_dict_file for dictionaries with no key-value pairs

--- vishnyakov.artiom/S4/utils.cpp
+++ vishnyakov.artiom/S4/utils.cpp
@@ -50,6 +50,39 @@ namespace vishnyakov
     return result;
   }
 
+  void parse_dict_file(std::istream& file, BSTree< std::string, Dictionary, std::less< std::string > >& dicts)
+  {
+    std::string line;
+
+    while (std::getline(file, line))
+    {
+      if (line.empty())
+      {
+        continue;
+      }
+
+      std::istringstream iss(line);
+      std::string dict_name;
+      iss >> dict_name;
+
+      if (dicts.has(dict_name))
+      {
+        throw std::runtime_error("Duplicate dictionary name");
+      }
+
+      Dictionary dict;
+      int key;
+      std::string value;
+
+      while (iss >> key >> value)
+      {
+        dict.push(key, value);
+      }
+
+      dicts.push(dict_name, dict);
+    }
+  }
+
   void process_commands(std::istream& in, BSTree< std::string, Dictionary, std::less< std::string > >& dicts, std::ostream& out)
   {
     std::string line;

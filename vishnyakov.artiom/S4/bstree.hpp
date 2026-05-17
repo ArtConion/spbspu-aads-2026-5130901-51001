3f91ad26a87340383efc8aa43296b993828fa413
ArtConion <artiom0408@mail.ru>
2026-05-17 21:14:22 +0300

    fix unit 2

--- vishnyakov.artiom/S4/bstree.hpp
+++ vishnyakov.artiom/S4/bstree.hpp
@@ -38,21 +38,24 @@ namespace vishnyakov
         left_(nullptr),
         right_(nullptr),
         parent_(parent)
-      {}
+      {
+      }
 
       Node(Key&& key, Value&& value, Node* parent = nullptr):
         data_(std::move(key), std::move(value)),
         left_(nullptr),
         right_(nullptr),
         parent_(parent)
-      {}
+      {
+      }
     };
 
     BSTree():
       root_(nullptr),
       size_(0),
       comp_()
-    {}
+    {
+    }
 
     BSTree(const BSTree& other):
       root_(nullptr),
@@ -99,7 +102,9 @@ namespace vishnyakov
     iterator begin() noexcept
     {
       if (!root_)
+      {
         return end();
+      }
 
       Node* current = root_;
       Stack< Node* > stack;
@@ -119,7 +124,9 @@ namespace vishnyakov
     const_iterator begin() const noexcept
     {
       if (!root_)
+      {
         return end();
+      }
 
       const Node* current = root_;
       Stack< const Node* > stack;
@@ -136,11 +143,25 @@ namespace vishnyakov
       return const_iterator(nullptr);
     }
 
-    const_iterator cbegin() const noexcept { return begin(); }
-    const_iterator cend() const noexcept { return end(); }
+    const_iterator cbegin() const noexcept
+    {
+      return begin();
+    }
 
-    bool empty() const noexcept { return size_ == 0; }
-    size_t size() const noexcept { return size_; }
+    const_iterator cend() const noexcept
+    {
+      return end();
+    }
+
+    bool empty() const noexcept
+    {
+      return size_ == 0;
+    }
+
+    size_t size() const noexcept
+    {
+      return size_;
+    }
 
     void push(const Key& key, const Value& value)
     {
@@ -158,18 +179,28 @@ namespace vishnyakov
       {
         parent = current;
         if (comp_(key, current->data_.first))
+        {
           current = current->left_;
+        }
         else if (comp_(current->data_.first, key))
+        {
           current = current->right_;
+        }
         else
+        {
           return;
+        }
       }
 
       Node* new_node = new Node(key, value, parent);
       if (comp_(key, parent->data_.first))
+      {
         parent->left_ = new_node;
+      }
       else
+      {
         parent->right_ = new_node;
+      }
       ++size_;
     }
 
@@ -189,28 +220,43 @@ namespace vishnyakov
       {
         parent = current;
         if (comp_(key, current->data_.first))
+        {
           current = current->left_;
+        }
         else if (comp_(current->data_.first, key))
+        {
           current = current->right_;
+        }
         else
+        {
           return;
+        }
       }
 
       Node* new_node = new Node(std::move(key), std::move(value), parent);
       if (comp_(key, parent->data_.first))
+      {
         parent->left_ = new_node;
+      }
       else
+      {
         parent->right_ = new_node;
+      }
       ++size_;
     }
 
-    bool has(const Key& key) const { return find_node(key) != nullptr; }
+    bool has(const Key& key) const
+    {
+      return find_node(key) != nullptr;
+    }
 
     Value& at(const Key& key)
     {
       Node* node = find_node(key);
       if (!node)
+      {
         throw std::out_of_range("Key not found");
+      }
       return node->data_.second;
     }
 
@@ -218,7 +264,9 @@ namespace vishnyakov
     {
       Node* node = find_node(key);
       if (!node)
+      {
         throw std::out_of_range("Key not found");
+      }
       return node->data_.second;
     }
 
@@ -226,7 +274,9 @@ namespace vishnyakov
     {
       Node* node = find_node(key);
       if (node)
+      {
         return node->data_.second;
+      }
       push(key, Value());
       node = find_node(key);
       return node->data_.second;
@@ -236,7 +286,9 @@ namespace vishnyakov
     {
       Node* node = find_node(key);
       if (!node)
+      {
         throw std::out_of_range("Key not found");
+      }
       Value result = std::move(node->data_.second);
       root_ = erase_node(root_, key);
       --size_;
@@ -259,7 +311,9 @@ namespace vishnyakov
     {
       Node* node = pos.node_;
       if (!node || !node->right_)
+      {
         return pos;
+      }
 
       Node* new_root = rotate_left_impl(node);
       return iterator(new_root, pos.stack_);
@@ -269,7 +323,9 @@ namespace vishnyakov
     {
       Node* node = pos.node_;
       if (!node || !node->left_)
+      {
         return pos;
+      }
 
       Node* new_root = rotate_right_impl(node);
       return iterator(new_root, pos.stack_);
@@ -279,7 +335,9 @@ namespace vishnyakov
     {
       Node* node = pos.node_;
       if (!node || !node->right_ || !node->right_->left_)
+      {
         return pos;
+      }
 
       Node* new_root = rotate_right_impl(node->right_);
       node->right_ = new_root;
@@ -292,7 +350,9 @@ namespace vishnyakov
     {
       Node* node = pos.node_;
       if (!node || !node->left_ || !node->left_->right_)
+      {
         return pos;
+      }
 
       Node* new_root = rotate_left_impl(node->left_);
       node->left_ = new_root;
@@ -301,12 +361,17 @@ namespace vishnyakov
       return iterator(new_root, pos.stack_);
     }
 
-    size_t height() const { return get_height(root_); }
+    size_t height() const
+    {
+      return get_height(root_);
+    }
 
     size_t height(const_iterator pos) const
     {
       if (pos.node_)
-        return get_height(const_cast<Node*>(pos.node_));
+      {
+        return get_height(const_cast< Node* >(pos.node_));
+      }
       return 0;
     }
 
@@ -324,7 +389,10 @@ namespace vishnyakov
       std::swap(comp_, other.comp_);
     }
 
-    Compare key_comp() const { return comp_; }
+    Compare key_comp() const
+    {
+      return comp_;
+    }
 
   private:
     Node* root_;
@@ -333,7 +401,10 @@ namespace vishnyakov
 
     Node* copy_tree(Node* node, Node* parent)
     {
-      if (!node) return nullptr;
+      if (!node)
+      {
+        return nullptr;
+      }
       Node* new_node = new Node(node->data_.first, node->data_.second, parent);
       new_node->left_ = copy_tree(node->left_, new_node);
       new_node->right_ = copy_tree(node->right_, new_node);
@@ -342,7 +413,10 @@ namespace vishnyakov
 
     void delete_tree(Node* node)
     {
-      if (!node) return;
+      if (!node)
+      {
+        return;
+      }
       delete_tree(node->left_);
       delete_tree(node->right_);
       delete node;
@@ -354,11 +428,17 @@ namespace vishnyakov
       while (current)
       {
         if (comp_(key, current->data_.first))
+        {
           current = current->left_;
+        }
         else if (comp_(current->data_.first, key))
+        {
           current = current->right_;
+        }
         else
+        {
           return current;
+        }
       }
       return nullptr;
     }
@@ -366,60 +446,85 @@ namespace vishnyakov
     Node* min_node(Node* node) const
     {
       while (node && node->left_)
+      {
         node = node->left_;
+      }
       return node;
     }
 
     Node* max_node(Node* node) const
     {
       while (node && node->right_)
+      {
         node = node->right_;
+      }
       return node;
     }
 
     size_t get_height(Node* node) const
     {
-      if (!node) return 0;
+      if (!node)
+      {
+        return 0;
+      }
       return 1 + std::max(get_height(node->left_), get_height(node->right_));
     }
 
     Node* erase_node(Node* node, const Key& key)
     {
-      if (!node) return nullptr;
+      if (!node)
+      {
+        return nullptr;
+      }
 
       if (comp_(key, node->data_.first))
       {
         node->left_ = erase_node(node->left_, key);
-        if (node->left_) node->left_->parent_ = node;
+        if (node->left_)
+        {
+          node->left_->parent_ = node;
+        }
       }
       else if (comp_(node->data_.first, key))
       {
         node->right_ = erase_node(node->right_, key);
-        if (node->right_) node->right_->parent_ = node;
+        if (node->right_)
+        {
+          node->right_->parent_ = node;
+        }
       }
       else
       {
         if (!node->left_)
         {
           Node* right = node->right_;
-          if (right) right->parent_ = node->parent_;
+          if (right)
+          {
+            right->parent_ = node->parent_;
+          }
           delete node;
           return right;
         }
         else if (!node->right_)
         {
           Node* left = node->left_;
-          if (left) left->parent_ = node->parent_;
+          if (left)
+          {
+            left->parent_ = node->parent_;
+          }
           delete node;
           return left;
         }
         else
         {
           Node* min = min_node(node->right_);
-          const_cast<Key&>(node->data_.first) = std::move(min->data_.first);
+          const_cast< Key& >(node->data_.first) = std::move(min->data_.first);
           node->data_.second = std::move(min->data_.second);
           node->right_ = erase_node(node->right_, min->data_.first);
-          if (node->right_) node->right_->parent_ = node;
+          if (node->right_)
+          {
+            node->right_->parent_ = node;
+          }
         }
       }
       return node;
@@ -430,16 +535,28 @@ namespace vishnyakov
       Node* right = node->right_;
       Node* parent = node->parent_;
       node->right_ = right->left_;
-      if (right->left_) right->left_->parent_ = node;
+      if (right->left_)
+      {
+        right->left_->parent_ = node;
+      }
       right->left_ = node;
       node->parent_ = right;
       right->parent_ = parent;
       if (parent)
       {
-        if (parent->left_ == node) parent->left_ = right;
-        else parent->right_ = right;
+        if (parent->left_ == node)
+        {
+          parent->left_ = right;
+        }
+        else
+        {
+          parent->right_ = right;
+        }
       }
-      else root_ = right;
+      else
+      {
+        root_ = right;
+      }
       return right;
     }
 
@@ -448,16 +565,28 @@ namespace vishnyakov
       Node* left = node->left_;
       Node* parent = node->parent_;
       node->left_ = left->right_;
-      if (left->right_) left->right_->parent_ = node;
+      if (left->right_)
+      {
+        left->right_->parent_ = node;
+      }
       left->right_ = node;
       node->parent_ = left;
       left->parent_ = parent;
       if (parent)
       {
-        if (parent->left_ == node) parent->left_ = left;
-        else parent->right_ = left;
+        if (parent->left_ == node)
+        {
+          parent->left_ = left;
+        }
+        else
+        {
+          parent->right_ = left;
+        }
       }
-      else root_ = left;
+      else
+      {
+        root_ = left;
+      }
       return left;
     }
   };
@@ -466,15 +595,28 @@ namespace vishnyakov
   class BSTIter
   {
     friend class BSTree< Key, Value, Compare >;
+    friend class BSTCIter< Key, Value, Compare >;
+
   public:
-    BSTIter() : node_(nullptr), stack_() {}
+    BSTIter():
+      node_(nullptr),
+      stack_()
+    {
+    }
 
     BSTIter(const BSTIter&) = default;
     BSTIter& operator=(const BSTIter&) = default;
     ~BSTIter() = default;
 
-    std::pair< const Key, Value >& operator*() { return node_->data_; }
-    std::pair< const Key, Value >* operator->() { return &node_->data_; }
+    std::pair< const Key, Value >& operator*()
+    {
+      return node_->data_;
+    }
+
+    std::pair< const Key, Value >* operator->()
+    {
+      return &node_->data_;
+    }
 
     BSTIter& operator++()
     {
@@ -505,35 +647,61 @@ namespace vishnyakov
       return tmp;
     }
 
-    bool operator==(const BSTIter& other) const { return node_ == other.node_; }
-    bool operator!=(const BSTIter& other) const { return !(*this == other); }
+    bool operator==(const BSTIter& other) const
+    {
+      return node_ == other.node_;
+    }
+
+    bool operator!=(const BSTIter& other) const
+    {
+      return !(*this == other);
+    }
 
   private:
     using Node = typename BSTree< Key, Value, Compare >::Node;
     Node* node_;
     Stack< Node* > stack_;
 
-    BSTIter(Node* node, const Stack< Node* >& stack = Stack< Node* >())
-      : node_(node), stack_(stack) {}
+    BSTIter(Node* node, const Stack< Node* >& stack = Stack< Node* >()):
+      node_(node),
+      stack_(stack)
+    {
+    }
   };
 
   template< class Key, class Value, class Compare >
   class BSTCIter
   {
     friend class BSTree< Key, Value, Compare >;
+
   public:
-    BSTCIter() : node_(nullptr), stack_() {}
+    BSTCIter():
+      node_(nullptr),
+      stack_()
+    {
+    }
 
     BSTCIter(const BSTCIter&) = default;
-    BSTCIter(const BSTIter< Key, Value, Compare >& other)
-      : node_(other.node_), stack_(other.stack_) {}
+
+    BSTCIter(const BSTIter< Key, Value, Compare >& other):
+      node_(other.node_),
+      stack_()
+    {
+    }
 
     ~BSTCIter() = default;
 
     BSTCIter& operator=(const BSTCIter&) = default;
 
-    const std::pair< const Key, Value >& operator*() const { return node_->data_; }
-    const std::pair< const Key, Value >* operator->() const { return &node_->data_; }
+    const std::pair< const Key, Value >& operator*() const
+    {
+      return node_->data_;
+    }
+
+    const std::pair< const Key, Value >* operator->() const
+    {
+      return &node_->data_;
+    }
 
     BSTCIter& operator++()
     {
@@ -564,16 +732,26 @@ namespace vishnyakov
       return tmp;
     }
 
-    bool operator==(const BSTCIter& other) const { return node_ == other.node_; }
-    bool operator!=(const BSTCIter& other) const { return !(*this == other); }
+    bool operator==(const BSTCIter& other) const
+    {
+      return node_ == other.node_;
+    }
+
+    bool operator!=(const BSTCIter& other) const
+    {
+      return !(*this == other);
+    }
 
   private:
     using Node = typename BSTree< Key, Value, Compare >::Node;
     const Node* node_;
     Stack< const Node* > stack_;
 
-    BSTCIter(const Node* node, const Stack< const Node* >& stack = Stack< const Node* >())
-      : node_(node), stack_(stack) {}
+    BSTCIter(const Node* node, const Stack< const Node* >& stack = Stack< const Node* >()):
+      node_(node),
+      stack_(stack)
+    {
+    }
   };
 
   template< class Key, class Value, class Compare >

8d77396dd24fb6d4d7c84c0f90975a143e954451
ArtConion <artiom0408@mail.ru>
2026-05-28 20:11:43 +0300

    add custom Vector class

--- vishnyakov.artiom/common/siphash.hpp
+++ vishnyakov.artiom/common/siphash.hpp
@@ -2,6 +2,9 @@
 #define SIPHASH_HPP
 
 #include <boost/container_hash/hash.hpp>
+#include <boost/hash2/get_integral_result.hpp>
+#include <boost/hash2/hash_append.hpp>
+#include <boost/hash2/siphash.hpp>
 #include <cstddef>
 #include <string>
 #include <utility>
@@ -11,13 +14,16 @@ namespace vishnyakov
   class SipHash
   {
   public:
-    template < class T >
+    template <class T>
     std::size_t operator()(const T& key) const
     {
-      return boost::hash< T >()(key);
+      boost::hash2::siphash_64 hasher{};
+      boost::hash2::hash_append(hasher, {}, key);
+      return boost::hash2::get_integral_result<std::size_t>(hasher);
     }
   };
 }
 
 #endif
+
 
